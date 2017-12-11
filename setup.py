da001542c45440fa56b4dcbdeda5f10756e8d6ac
Marcel Röthke <marcel@roethke.info>
2017-12-11 20:17:01 +0100

    Prepare (pypi) release

--- setup.py
+++ setup.py
@@ -1,15 +1,27 @@
 #!/usr/bin/env python3
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
+import radicale_dovecot_auth
 
 setup(
-        name="radicale-dovecot-auth",
-        version='0.1',
-        description="Dovecot authentication plugin for Radicale",
-        author="Arvedui",
-        author_email="arvedui@posteo.de",
-        url="http://github.com/Arvedui/radicale-dovecot-auth",
-        license="GNU GPL v3",
-        platforms="Any",
-        packages=["radicale_dovecot_auth"])
+    name='radicale_docecot_auth',
+    version=radicale_dovecot_auth.__version__,
+    description="Dovecot authentication plugin for Radicale",
+    url='https://github.com/Arvedui/radicale-dovecot-auth',
+    author='Arvedui',
+    author_email='arvedui@posteo.de',
+    license="GNU GPL v3",
+    packages=["radicale_dovecot_auth"],
+    install_requires=['radicale'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Operating System :: POSIX',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
+        ]
+    )
