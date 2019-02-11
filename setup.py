a43518b166e5d66c052662dcb674bf14d222b4bb
Arvedui <arvedui@posteo.de>
2019-02-12 00:25:31 +0100

    improve coding style of setup.py

--- setup.py
+++ setup.py
@@ -1,27 +1,28 @@
 #!/usr/bin/env python3
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
 import radicale_dovecot_auth
 
 setup(
-    name='radicale_dovecot_auth',
-    version=radicale_dovecot_auth.__version__,
-    description="Dovecot authentication plugin for Radicale",
-    url='https://github.com/Arvedui/radicale-dovecot-auth',
-    author='Arvedui',
-    author_email='arvedui@posteo.de',
-    license="GNU GPL v3",
-    packages=["radicale_dovecot_auth"],
-    install_requires=['radicale'],
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Operating System :: POSIX',
-        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6'
-        ]
-    )
+        name='radicale_dovecot_auth',
+        version=radicale_dovecot_auth.__version__,
+        description="Dovecot authentication plugin for Radicale",
+        url='https://github.com/Arvedui/radicale-dovecot-auth',
+        author='Arvedui',
+        author_email='arvedui@posteo.de',
+        license="GNU GPL v3",
+        packages=["radicale_dovecot_auth"],
+        install_requires=['radicale'],
+        classifiers=[
+                'Development Status :: 5 - Production/Stable',
+                'Operating System :: POSIX',
+                'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+                'Programming Language :: Python :: 3',
+                'Programming Language :: Python :: 3.3',
+                'Programming Language :: Python :: 3.4',
+                'Programming Language :: Python :: 3.5',
+                'Programming Language :: Python :: 3.6',
+                'Programming Language :: Python :: 3.8'
+                ]
+        )
