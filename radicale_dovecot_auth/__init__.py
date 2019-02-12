f26b9cebab8a914a576bf370d80ecd353402563d
Arvedui <arvedui@posteo.de>
2019-02-12 21:45:44 +0100

    remove unused import

--- radicale_dovecot_auth/__init__.py
+++ radicale_dovecot_auth/__init__.py
@@ -14,9 +14,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-
-import socket
-
 from radicale.auth import BaseAuth
 
 from radicale_dovecot_auth.dovecot_auth import DovecotAuth
